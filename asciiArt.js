dc758000d5d2a91aa9e9fb9e9607b457399f9019
ryandoeringOSU <doerinry@oregonstate.edu>
2024-04-15 18:06:48 -0700

    Added Figlet to asciiArt.js

--- asciiArt.js
+++ asciiArt.js
@@ -1,3 +1,12 @@
+const figlet = require("figlet");
 
 const inputString = process.argv.slice(2).join(" ");    
 console.log("Input string:", inputString);
+figlet(inputString, function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
